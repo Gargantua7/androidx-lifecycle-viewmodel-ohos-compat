367fcaffc4f2936db6128a11f7eddab2bb7edde3
Gargantua7 <gargantua7@qq.com>
2025-09-25 19:10:29 +0800

    Resolve errors

--- lifecycle/lifecycle-viewmodel-compose/src/commonMain/kotlin/androidx/lifecycle/viewmodel/compose/ViewModel.kt
+++ lifecycle/lifecycle-viewmodel-compose/src/commonMain/kotlin/androidx/lifecycle/viewmodel/compose/ViewModel.kt
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@file:JvmName("ViewModelKt")
-@file:JvmMultifileClass
 
 package androidx.lifecycle.viewmodel.compose
 
